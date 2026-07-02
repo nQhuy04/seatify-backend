f9ab532de086adda6258a72382c79433ffe2428c
Huy Quang <huyho093@gmail.com>
2026-07-02 10:27:44 +0700

    chore: setup initial backend architecture with Node, TS, and Prisma

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Nạp biến môi trường từ file .env
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors()); // Cho phép Frontend gọi API không bị chặn
+app.use(express.json()); // Cho phép Backend đọc data JSON từ Client gửi lên
+
+// Route cơ bản để test
+app.get('/', (req: Request, res: Response) => {
+  res.send('Chào mừng đến với API Backend của Seatify!');
+});
+
+// Lắng nghe cổng
+app.listen(PORT, () => {
+  console.log(`Server Seatify đang chạy thành công tại http://localhost:${PORT}`);
+});
